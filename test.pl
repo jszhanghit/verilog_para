4a80d67b01356dca3ce9715446e3eb7186e216c2
Jszhanghit <31990464+jszhanghit@users.noreply.github.com>
2022-09-06 21:40:28 +0800

    测试模块

---
+++ test.pl
@@ -0,0 +1,7 @@
+use lib 'D:\verilog_para' ;
+use Verilog_para ;
+$\="\n" ;
+my $a=' fdfgg /* fddfgf//   */fds  dfsdf */ dfssfds// dffsf\* fdfsgsd*\ fsd dfss//dfsdfds';
+print($a) ;
+print del_comment($a,1) ;
+&get_module("fddsaf",1,2);
