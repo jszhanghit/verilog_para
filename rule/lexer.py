c7b6b28c7f8f666a9ff174749f71883d88e5e111
Jszhanghit <31990464+jszhanghit@users.noreply.github.com>
2022-09-30 15:40:20 +0800

    Update lexer.py

--- rule/lexer.py
+++ rule/lexer.py
@@ -5,14 +5,15 @@ from ply import lex
 class Vlexer(object):
     def build(self, **kwargs):
         self.lexer = lex.lex(object=self, **kwargs)
-    keyword = ("MODULE","ENDMODULE","PARAMETER")
+    keyword = ("MODULE","ENDMODULE","PARAMETER","WIRE","LOGIC",
+               "REG","SIGNED","UNSIGNED","INPUT","OUTPUT")
     reserved = {}
     for kw in keyword:
         reserved[kw.lower()] = kw
     tokens = keyword + (
-        "ID",
+        "ID","NUMBER",
         
-        "LPAREN","RPAREN","COMMA",
+        "LPAREN","RPAREN","COMMA","LBRAC","RBRAC","COLON",
         #操作符
         "ADD","SUB","MUL","DIV"
     )
@@ -20,12 +21,19 @@ class Vlexer(object):
     t_LPAREN = r"\("
     t_RPAREN = r"\)"
     t_COMMA = r","
+    t_LBRAC = r"\["
+    t_RBRAC = r"\]"
+    t_COLON = r":"
+    def t_NUMBER(self,t):
+        r"\d+"
+        t.value = int(t.value)
+        return t
     def t_NEWLINE(self,t):
         r"\n"
         pass
 
     def t_error(self,t):
-        print("Illegal "+t.value)
+        print("Illegal "+t.value[0])
         self.lexer.skip(1)
     
     def t_ID(self,t):
